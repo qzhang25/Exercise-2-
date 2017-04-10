28f6aae205516612c7bebcc12a1b6dbaa0eb27b7
qzhang25 <zhangqing198573@gmail.com>
2017-04-09 19:00:36 -0700

    Update parse.py

--- extweetwordcount/src/bolts/parse.py
+++ extweetwordcount/src/bolts/parse.py
@@ -1,4 +1,17 @@
-def process(self, tup):
+from __future__ import absolute_import, print_function, unicode_literals
+
+import re
+from streamparse.bolt import Bolt
+
+################################################################################
+# Function to check if the string contains only ascii chars
+################################################################################
+def ascii_string(s):
+  return all(ord(c) < 128 for c in s)
+
+class ParseTweet(Bolt):
+
+    def process(self, tup):
         tweet = tup.values[0]  # extract the tweet
 
         # Split the tweet into words
@@ -31,3 +44,5 @@ def process(self, tup):
 
         # Emit all the words
         self.emit_many(valid_words)
+
+        # tuple acknowledgement is handled automatically
